629bc60e83bd809dbe55abb06703cf607aa92931
LukeKang29183618 <161133567+LukeKang29183618@users.noreply.github.com>
2024-02-24 21:58:14 +0900

    Create Test_20240224.c

---
+++ Test_20240224.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+int main() 
+{
+    int a, b, c;
+    
+    printf("원하는 숫자 입력하시오 : ");
+    scanf("%d", &a);
+    b = 2;
+    c = a + b;
+    
+
+    printf("%d", c);
+    return 0;
+}
